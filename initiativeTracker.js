de765f20e39b38f740785034c2edeccd897839ea
lmarlow2 <47681639+lmarlow2@users.noreply.github.com>
2019-02-16 18:43:16 -0600

    Update initiativeTracker.js

--- initiativeTracker.js
+++ initiativeTracker.js
@@ -1,3 +1,13 @@
-function addToInitiativeOrder(form){
+var initiativeOrder = [];
+
+function compare(a,b){
+  return a.value - b.value;
+}
+
+function addToInitiativeOrder(){
   console.log("Test.");
 }
+
+function resetInitiativeOrder(){}
+
+function removeFromInitiativeOrder(){}
