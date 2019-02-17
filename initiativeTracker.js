c7f57f0e1f2c2732f251e63c267c0b117f984aa8
lmarlow2 <47681639+lmarlow2@users.noreply.github.com>
2019-02-16 19:22:24 -0600

    Update initiativeTracker.js

--- initiativeTracker.js
+++ initiativeTracker.js
@@ -3,6 +3,9 @@ var initiativeOrder = [];
 function compare(a,b){ return b.initiative - a.initiative; }
 
 function addToInitiativeOrder(){
+  if(document.getElementById("roll").checked){
+    document.getElementById("initiative").value = Math.floor(Math.random() * 20);
+  }
   initiativeOrder.push({name:document.getElementById("name").value, initiative:document.getElementById("initiative").value});
   initiativeOrder.sort(compare);
   var list = "";
@@ -10,7 +13,6 @@ function addToInitiativeOrder(){
     list += "<li>" + initiativeOrder[k].name + "</li>";
   }
   document.getElementById("initiativeList").innerHTML = list;
-  console.log(initiativeOrder);
 }
 
 function resetInitiativeOrder(){
